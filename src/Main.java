c6af39047d29ba6ac784b4abc56214c8d974c5c9
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 11:05:36 -0500

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
                 System.out.println(playerA + " is not a valid move.");
             }
             while(true) {
-                System.out.println("Player 2 please enter your move (r, p, or s)");
+                System.out.println("Player 2 please enter your move (r, p, or s)"); //asking second player to enter move
                 playerB = scanner.nextLine();
                 if (playerB.equals("r") || playerB.equals("p") || playerB.equals("s")) {
                     break;
@@ -26,39 +26,39 @@ public class Main {
                 System.out.println(playerB + " is not a valid move."); //If in proper letter is entered this is what is seen
             }
             if (playerA.equals(playerB)) {
-                System.out.println("The game was a tie!");
+                System.out.println("The game was a tie!"); //if both players play the same thing
             }
-            else if (playerA.equals("r")) {
+            else if (playerA.equals("r")) { //this is taking what each player plays
                 if (playerB.equals("p")) {
-                    System.out.println("Player 2 wins!");
+                    System.out.println("Paper covers Rock Player 2 wins!");
 
                 } else if (playerB.equals("s")) {
-                    System.out.println("Player 1 wins!");
+                    System.out.println("Rock smashes Scissors Player 1 wins!");
                 }
             }
 
             else if (playerA.equals("p")) {
                 if (playerB.equals("r")) {
-                    System.out.println("Player 1 wins!");
+                    System.out.println("Paper covers Rock Player 1 wins!");
 
                 } else if (playerB.equals("s")) {
-                    System.out.println("Player 2 wins!");
+                    System.out.println("Scissors cuts paper Player 2 wins!");
                 }
             }
 
             else if (playerA.equals("s")) {
                 if (playerB.equals("p")) {
-                    System.out.println("Player 1 wins!");
+                    System.out.println("Scissors cuts paper Player 1 wins!");
 
                 } else if (playerB.equals("r")) {
-                    System.out.println("Player 2 wins!");
+                    System.out.println("Rock smashes scissors Player 2 wins!");
                 }
             }
 
-            System.out.println("Play again? (y/n)");
+            System.out.println("Play again? (y/n)"); //this is question if they want to play again
             String playAgain = scanner.nextLine();
 
-            if (!playAgain.equals("y")) {
+            if (!playAgain.equals("y")) { //if yes it repeats back to start again
                 break;
             }
         }
